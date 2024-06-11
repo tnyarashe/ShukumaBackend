ee880fe60ca40ea6d047ba10323de88a2f02773b
Tsepo <3tnyarashe@gmail.com>
2024-06-11 13:07:05 +0200

    added product code in sever file and "product routes"

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,13 +1,11 @@
 const express = require("express")
 const router = express.Router()
 const path =  require('path')
-const user = require('../controllers/users.controller')
-const auth = require('../middleware/auth.middleware')
-const product = require("../models/product_model")
-
+const user = require('../product/product_controller')
+// const auth = require('../middleware/auth.middleware')
 router.get('/all', product.getAllProducts)
 router.get('/product', product.getOneProduct)
-router.put('/product/:id', product.updateOneProduct)
+router.post('/product/:id', product.updateOneProduct)
 // router.get("/user",auth.verifyToken,user.user_dBoard);
 // router.get("/admin",[auth.verifyToken, auth.isAdmin],user.admin_dBoard);
 
--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const db = require('./config/db.config')
 const mongoose = require('mongoose')
 const userRoutes  = require('./routes/user.routes')
 const authRoutes  = require('./routes/auth.routes')
+const productRoutes = require('./routes/product.routes')
 // const orderRoutes = require('./routes/orderRoutes')
 // const order_model = require('./models/order_model');
 const path = require('path')
 const dotenv = require('dotenv');
-const { default: cartRoutes } = require('./routes/cartRoutes')
+// const { default: cartRoutes } = require('./routes/cartRoutes')
 
 // Set up Global configuration access
 dotenv.config();
@@ -37,6 +38,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use('/v1/users', userRoutes);
 app.use('/v1/auth ', authRoutes);
+app.use('/v1/products', productRoutes)
 // app.use('/v1/orders',orderRoutes);
 // app.use('/v1/cart', cartRoutes)
 
@@ -53,7 +55,6 @@ app.get('/signup', (req, res)=>{
 } )
 
 //Render the login page
-
 app.get('/login', (req, res)=>{
     res.sendFile(path.join(__dirname,'public','login.html'))
 } )
