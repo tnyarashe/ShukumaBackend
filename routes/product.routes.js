736496d24ec8fbc0a63470c3365914e2bc01ef75
Tsepo <3tnyarashe@gmail.com>
2024-06-18 13:18:48 +0200

    updaete

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const products = require('../controllers/product_controller')
 
 router.post('/add-product', products.createProduct)
-router.get('/all', products.getAllProducts)
+router.get('/get-all', products.getAllProducts)
 router.get('/get-one/:id', products.getOneProduct)
 router.delete('/delete-one/:id', products.deleteProduct)
 router.delete('/delete-all', products.deleteAllProducts)
