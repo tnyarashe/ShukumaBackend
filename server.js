44e3fa2b98bab6d07d3f03abc0290fbd1c3f5447
Tsepo <3tnyarashe@gmail.com>
2024-06-21 15:51:04 +0200

    update

--- server.js
+++ server.js
@@ -66,4 +66,4 @@ app.listen(process.env.PORT, ()=>{
     console.log("Listening @ port:", process.env.PORT)
 })
 
-mongodb+srv://3tnyarashe:ShukumaDB@shukumadb.ccfdzls.mongodb.net/
+// mongodb+srv://3tnyarashe:ShukumaDB@shukumadb.ccfdzls.mongodb.net/
