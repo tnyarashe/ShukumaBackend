966ccddfc1bad0325b28673e97f6721652b2a561
DiniJNS <jonas.dinilethu@gmail.com>
2024-06-21 15:50:22 +0200

    added order schema

--- models/order_model.js
+++ models/order_model.js
@@ -47,3 +47,54 @@
 // const order = mongoose.model('order', orderSchema);
 
 // module.exports = order;
+
+const mongoose = require('mongoose');
+const orderSchema = mongoose.Schema({
+    description:{
+        type:String,
+        required:true
+    },
+
+    total:{
+        type:Number,
+        required:true
+    },
+
+    user:{
+        type:String,
+        required: true
+
+    },
+
+    address:{
+        street:{type:String},
+        city:{type:String},
+        postalcode:{type:String},
+        country:{type:String},
+
+    },
+    
+    items:{
+        type:String,
+        required:true
+    },
+
+    orderNumber:{
+        type:Number,
+        required:true
+    },
+
+    
+},{
+    timestamps:true
+    
+})
+orderSchema.method('to JSON', function(){
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+})
+
+
+const Order = mongoose.model('order', orderSchema)
+module.exports = Order
