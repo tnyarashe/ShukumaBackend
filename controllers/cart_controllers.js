74d6889188a75e96e5875618544ac4a6c9aaf2e7
Tsepo <3tnyarashe@gmail.com>
2024-07-02 16:37:31 +0200

    added removeFromCart controller

--- controllers/cart_controllers.js
+++ controllers/cart_controllers.js
@@ -38,66 +38,22 @@ exports.addToCart = async (req, res) => {
   }
 };
 
-// exports.removeFromCart = async (req, res) => {
-//   try {
-//     const cart = await Cart.findById(req.params.cartId);
-//     if (!cart) {
-//       return res.status(404).json({ message: 'Cart not found' });
-//     }
+exports.removeFromCart = async (req, res) => {
+  try {
+    const cart = await Cart.findById(req.params.cartId);
+    if (!cart) {
+      return res.status(404).json({ message: 'Cart not found' });
+    }
 
-//     cart.items = cart.items.filter(item => !item.productId.equals(req.params.productId));
-//     cart.totalPrice = cart.items.reduce((total, item) => total + item.quantity * item.price, 0);
+    cart.items = cart.items.filter(item => !item.productId.equals(req.params.productId));
+    cart.totalPrice = cart.items.reduce((total, item) => total + item.quantity * item.price, 0);
 
-//     await cart.save();
-//     res.json(cart);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// // };
-
-
-// exports.addToCart = async (req, res) => {
-//   const { productId, quantity, price } = req.body;
-
-//   try {
-//     let cart = await Cart.findById(req.params.cartId);
-//     if (!cart) {
-//       cart = new Cart();
-//     }
-
-//     const itemIndex = cart.items.findIndex(item => item.productId.equals(productId));
-//     if (itemIndex > -1) {
-//       cart.items[itemIndex].quantity += quantity;
-//       cart.items[itemIndex].price = price;
-//     } else {
-//       cart.items.push({ productId, quantity, price });
-//     }
-
-//     cart.totalPrice = cart.items.reduce((total, item) => total + item.quantity * item.price, 0);
-
-//     await cart.save();
-//     res.status(201).json(cart);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// };
-
-// exports.removeFromCart = async (req, res) => {
-//   try {
-//     const cart = await Cart.findById(req.params.cartId);
-//     if (!cart) {
-//       return res.status(404).json({ message: 'Cart not found' });
-//     }
-
-//     cart.items = cart.items.filter(item => !item.productId.equals(req.params.productId));
-//     cart.totalPrice = cart.items.reduce((total, item) => total + item.quantity * item.price, 0);
-
-//     await cart.save();
-//     res.json(cart);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// };
+    await cart.save();
+    res.json(cart);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 // exports.clearCart = async (req, res) => {
 //   try {
