2117466e85545d25e284c27f452ab05429058bbe
Tsepo <3tnyarashe@gmail.com>
2024-07-03 15:04:23 +0200

    update

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -2,9 +2,6 @@ const express = require('express');
 const order = require('../models/order_model');
 const router = express.Router();
 
-router.post('/add', order.placeOrder)
-
-router.get('all', order.getAllOrders)
 router.delete('/orders/:id', orderController.deleteOrder);
 router.delete('/orders/', orderController.deleteAllOrders);
 
