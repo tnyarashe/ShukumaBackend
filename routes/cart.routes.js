5991abf10d656e9ba3f8dcd9b8e67948a0d0c0a4
add coments <lukhanyo95@gmail.com>
2024-07-04 17:16:16 +0200

    correct spelling in route

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const cartController = require('../controllers/cart_controllers');
 
-router.get('/alle', cartController.getAllCart)
+router.get('/all', cartController.getAllCart)
 router.post('/add/', cartController.addToCart);
 router.delete('/:cartId/:productId', cartController.removeFromCart);
 router.delete('/:cartId', cartController.clearCart);
