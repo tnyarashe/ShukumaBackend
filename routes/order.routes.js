532148c794ca824b268b4758e870fd6e9abfd5d8
SfisoNxumalo <sfisonxumalo69@gmail.com>
2024-07-12 20:42:47 +0200

    finished

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -5,6 +5,8 @@ const orderController = require('../controllers/order_controller');
 router.post('/', orderController.createOrder);
 router.delete('/:id', orderController.deleteOrder);
 router.delete('/', orderController.deleteAllOrders);
-router.get('/', orderController.getAllOrders )
+router.get('/:userid', orderController.getAllOrders)
+router.get('/driver/:driverid', orderController.getAllDriverOrders)
 router.get('/one', orderController.getOrderById)
+router.put('/update/:id', orderController.updateOrder)
 module.exports = router;
